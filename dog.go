873da3cefaf9f26d66b3a20bdae8654d96704ddd
Andrii Hruzdiev <a.hruzdiev@autodoc.eu>
2024-09-15 19:07:44 +0300

    method of v1.0.0

--- dog.go
+++ dog.go
@@ -5,3 +5,7 @@ import "strings"
 func WhenGrownUp(s string) string {
 	return "When the pussy grows up it says: " + strings.ToUpper(s)
 }
+
+func From10() string {
+	return "Code of version v.1.0.0"
+}
